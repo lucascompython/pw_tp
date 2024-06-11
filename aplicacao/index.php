8817aa189c6b0be223fdaff487cd01b1e62f896e
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-06-11 22:19:36 +0100

    Change the layout of the app page and add time info

--- aplicacao/index.php
+++ aplicacao/index.php
@@ -12,6 +12,8 @@ $utilizador = utilizador();
 # CARREGA O CABECALHO PADRÃO COM O TÍTULO
 $titulo = '- Aplicação';
 include_once __DIR__ . '/templates/cabecalho.php';
+
+date_default_timezone_set("Europe/Lisbon");
 ?>
 
 <body>
@@ -33,47 +35,54 @@ include_once __DIR__ . '/templates/cabecalho.php';
                 </div>
             </header>
 
-            <div class="p-5 mb-4 bg-body-tertiary rounded-3 position-relative">
-                <img src="<?= !empty($utilizador['foto']) ? '/recursos/imagens/uploads/' . $utilizador['foto'] : '/recursos/imagens/default_pfp.svg' ?>" alt="Profile Picture" class="m-3 rounded-circle card-img-top position-absolute top-0 end-0" style="width: 100px; height: 100px;">
-
-                <div class="container-fluid py-5">
-                    <div>
-                        <h1 class="display-5 fw-bold">Olá <?= $utilizador['nome'] ?? null ?> !</h1>
-                        <p class="col-md-8 fs-4">Agora que está logado no sistema, você tem acesso a informações exclusivas.</p>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="p-3 mb-4 bg-body-tertiary rounded-3 position-relative">
+                        <div class="container-fluid">
+                            <div>
+                                <div class="d-flex align-items-center justify-content-between border-bottom mb-5">
+                                    <h3 class="fw-bold">Olá <?= $utilizador['nome'] ?? null ?> !</h3>
+                                    <img src="<?= !empty($utilizador['foto']) ? '/recursos/imagens/uploads/' . $utilizador['foto'] : '/recursos/imagens/default_pfp.svg' ?>" alt="Profile Picture" class="m-3 rounded-circle card-img-top " style="width: 100px; height: 100px;">
+                                </div>
+                                <p class="fs-6">Agora que está logado no sistema, você tem acesso a informações exclusivas.</p>
+                                <p class="fs-5">Hora Atual em Portugal: <?= date('H:i:s') ?></p>
+                                <p class="fs-5">Hora Atual em Espanha: <?= date('H:i:s', strtotime('+1 hour')) ?></p>
+                            </div>
+                            <div class="d-flex justify-content">
+                                <form action="/src/controlador/aplicacao/controlar-autenticacao.php" method="post" class="flex-fill mt-5">
+                                    <button class="btn btn-danger btn-lg px-4 w-100" type="submit" name="utilizador" value="logout">Logout</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div class="d-flex justify-content">
-                        <form action="/src/controlador/aplicacao/controlar-autenticacao.php" method="post">
-                            <button class="btn btn-danger btn-lg px-4" type="submit" name="utilizador" value="logout">Logout</button>
-                        </form>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-12 mb-4">
+                            <div class="h-100 p-5 text-bg-dark rounded-3">
+                                <h2>Perfil</h2>
+                                <p>Este painel é utilizado para destacar que utilizadores REGISTADOS podem alterar o próprio perfil. Aqui você poderá editar e cancelar a sua assinatura no sistema. <strong class="text-warning">E se estiver vendo este parágrafo, significa que você precisa completar o seu perfil.</strong></p>
+                                <a href="/aplicacao/perfil.php"><button class="btn btn-outline-light px-5" type="button">Editar</button></a>
+                            </div>
+                        </div>
+
+                        <?php
+                        # MOSTRA CARD APENAS SE UTILIZDOR FOR ADMINISTRADOR
+                        if (autenticado() && $utilizador['administrador']) {
+                            echo '<div class="col-md-12">
+                <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+                    <h2>Painel de Administração</h2>
+                    <p>Este painel é exclusivo para utilizadores REGISTADOS e que tenham o perfil de ADMINISTRADOR. Aqui você poderá criar, alterar, apagar, promover e despromover outros utilizadores a administradores do sistema.</p>
+                    <a href="/admin/"><button class="btn btn-outline-success" type="button">Administração</button></a>
+                </div>
+            </div>';
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
 
-            <div class="row align-items-md-stretch">
-                <div class="col-md-6">
-                    <div class="h-100 p-5 text-bg-dark rounded-3">
-                        <h2>Perfil</h2>
-                        <p>Este painel é utilizado para destacar que utilizadores REGISTADOS podem alterar o próprio perfil. Aqui
-                            você poderá editar e cancelar a sua assinatura no sistema. <strong class="text-warning">E se estiver vendo
-                                este parágrafo, significa que você precisa completar o seu perfil.</strong></p>
-                        <a href="/aplicacao/perfil.php"><button class="btn btn-outline-light px-5" type="button">Editar</button></a>
-                    </div>
-                </div>
-
-                <?php
-                # MOSTRA CARD APENAS SE UTILIZDOR FOR ADMINISTRADOR
-                if (autenticado() && $utilizador['administrador']) {
-                    echo '<div class="col-md-6">
-                    <div class="h-100 p-5 bg-body-tertiary border rounded-3">
-                        <h2>Painel de Administração</h2>
-                        <p>Este painel é exclusivo para utilizadores REGISTADOS e que tenham o perfil de ADMINISTRADOR. Aqui você poderá criar,
-                            alterar, apagar, promover e despromover outros utilizadores a administradores do sistema.</p>
-                        <a href="/admin/"><button class="btn btn-outline-success" type="button">Administração</button></a>
-                    </div>
-                </div>';
-                }
-                ?>
-            </div>
             <?php
             include_once __DIR__ . '/templates/rodape.php';
             ?>
