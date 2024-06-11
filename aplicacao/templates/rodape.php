7790fe447639f9d2aa2fce3969cd7a1859d9afbc
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-06-11 21:14:03 +0100

    Update bootstrap to not use the alpha version

--- aplicacao/templates/rodape.php
+++ aplicacao/templates/rodape.php
@@ -3,7 +3,7 @@
         <p class="mt-3 mb-3 text-body-secondary border-top">IPVC ESTG - Desenvolvimento WEB | Lucas de Linhares &copy; - 2023-2024</p>
       </article>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </div>
     </body>
 
