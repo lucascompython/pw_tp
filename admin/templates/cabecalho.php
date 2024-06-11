da6da1979b51dd39635577184e4560006b5d693a
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-06-11 21:12:34 +0100

    Update the title

--- admin/templates/cabecalho.php
+++ admin/templates/cabecalho.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <title>CRUD<?= $titulo ?? NULL ?></title>
+  <title>Administração Paulo Ministro<?= $titulo ?? NULL ?></title>
 </head>
 
 <body class="container bg-light">
@@ -15,4 +15,3 @@
       <h1>Registo de Utilizadores</h1>
       <p>CRUD | Front-end Bootstrap | Back-end PHP</p>
     </div>
-    
