04ec57d129a1cdd0a3fb10ce8b44f40a0ce993d4
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-06-11 20:49:30 +0100

    Change the name in the footer

--- aplicacao/templates/rodape.php
+++ aplicacao/templates/rodape.php
@@ -1,6 +1,6 @@
     <footer>
       <article>
-        <p class="mt-3 mb-3 text-body-secondary border-top">IPVC ESTG - Desenvolvimento WEB | Professor Wenderson Wanzeller &copy; - 2023-2024</p>
+        <p class="mt-3 mb-3 text-body-secondary border-top">IPVC ESTG - Desenvolvimento WEB | Lucas de Linhares &copy; - 2023-2024</p>
       </article>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
